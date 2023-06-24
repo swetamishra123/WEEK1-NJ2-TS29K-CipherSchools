d243c415abf0a729094d0af154ebf4c776bc09b9
swetamishra123 <swetamishra603@gmail.com>
2023-06-24 23:47:39 +0530

    initial

--- Lecture 7 - React - II/myapp2/src/App.js
+++ Lecture 7 - React - II/myapp2/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+//Alias
+import Pizza from "./components/Pizza";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>My first react app</h1>
+//     </div>
+//   );
+// }
+
+//Class based Component
+class App extends Component{
+  render(){
+    let dynamicName = "React is mind blowing";
+    let brand = "PizzaHut"
+    let description = "Best Pizza in town";
+
+    let brand2 = "Domino's"
+    let description2 = "Best Pizza in town";
+
+    return(
+      <div>
+        <h1>{dynamicName}</h1>
+        <Pizza brand={brand} description={description} />
+        <Pizza brand={brand2} description={description2} />
+      </div>
+    );
+  }
 }
-
 export default App;
